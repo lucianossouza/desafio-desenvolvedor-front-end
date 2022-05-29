e4a0bdc897431cc2899112ac8bb86147bd65de7c
lucianossouza <luciano.illustrador@hotmail.com>
2022-05-29 18:59:32 -0300

    adicionando melhorias no HTML, CSS e JS

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ O desafio consiste no desenvolvimento do layout de uma landing page com uma nota
 
 - Utilizar algumas variáveis ​​CSS para agilizar e padronizar
 - Completar o desenvolvimento da página antes de iniciar os scripts
+- Adicionar JavaScript em um arquivo separado
 
 ## Como usar
 
--- validcpf.js
+++ validcpf.js
@@ -3,25 +3,6 @@ const isValidCPF = (number) => {
   cpf = cpf.replace(/[\s.-]*/igm, '')
   
   if (cpf.length !== 11 || !Array.from(cpf).filter(e => e !== cpf[0]).length) {
-      return false
-}
-
-  let sum;
-  let rest;
-  sum = 0;
-  if (number == "00000000000") return false;
-
-  for (i=1; i<=9; i++) sum = sum + parseInt(number.substring(i-1, i)) * (11 - i);
-  rest = (sum * 10) % 11;
-
-  if ((rest == 10) || (rest == 11))  rest = 0;
-  if (rest != parseInt(number.substring(9, 10)) ) return false;
-
-  sum = 0;
-  for (i = 1; i <= 10; i++) sum = sum + parseInt(number.substring(i-1, i)) * (12 - i);
-  rest = (sum * 10) % 11;
-
-  if ((rest == 10) || (rest == 11))  rest = 0;
-  if (rest != parseInt(number.substring(10, 11) ) ) return false;
-  return true;
+    return false
+  }
 }
